6075e2b220873750ab835bbdff011bf78e0e3553
Aniket Singh <aniketsingh90126@gmail.com>
2025-01-21 10:51:00 +0530

    feat: crating a simple static website

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = [
+  {
+    name: "Aniket singh",
+    email: "aniketsingh90126@gmail.com",
+  },
+  {
+    name: "Julesh pun",
+    email: "juleshpun@gmail.com",
+  },
+  {
+    name: "Nikhil upadhyay",
+    email: "nikhilupadhyay@gmail.com",
+  },
+  {
+    name: "Hemant Sharma",
+    email: "hemantsharma@gmail.com",
+  },
+];
+
+module.exports = data;
