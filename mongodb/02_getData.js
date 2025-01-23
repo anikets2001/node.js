d6d9514ec3983f7f291f47740bca5a3002384769
Aniket Singh <aniketsingh90126@gmail.com>
2025-01-23 11:57:04 +0530

    feat: insert data into mongodb with node

---
+++ mongodb/02_getData.js
@@ -0,0 +1,19 @@
+
+const dbConnect = require('./01_connectMongodb')
+
+// with .then/.catch
+// dbConnect().then((response) => {
+//   response
+//     .find()
+//     .toArray()
+//     .then((data) => console.log(data));
+// });
+
+// with async/await
+const getData = async()=> {
+  let response = await dbConnect();
+  let data = await response.find().toArray();
+  console.log(data)
+}
+
+getData()
